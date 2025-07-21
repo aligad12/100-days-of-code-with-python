0c7dd4db6bccf44d1293ad3ed422f0e7795caf90
Ali Gad <aliahmedgad2005@gmail.com>
2025-07-21 15:47:52 +0300

    update for romberg's world part 4
    fixed the counter initial point to zero to make more sense and fixed the range function in the for loop instead

--- day6/romberg's_world_part4.py
+++ day6/romberg's_world_part4.py
@@ -2,25 +2,28 @@ def turn_right():
     turn_left()
     turn_left()
     turn_left()
-    
+
+
 def turn_around():
     turn_left()
     turn_left()
-    
+
+
 def jump():
     turn_left()
     move()
-    counter = 1
+    counter = 0
     while wall_on_right():
-        counter+=1
+        counter += 1
         move()
     turn_right()
     move()
     turn_right()
-    for count in range(counter):
+    for count in range(counter + 1):
         move()
     turn_left()
-    
+
+
 while not at_goal():
     if front_is_clear():
         move()
