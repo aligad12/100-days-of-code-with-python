9e83831e83e20c036307dcf6505aac389573ca82
Ali Gad <aliahmedgad2005@gmail.com>
2025-07-30 03:04:45 +0300

    Coffee_machine_program

--- day16/main.py
+++ day16/main.py
@@ -1,3 +1,27 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+is_machine_on = True
+drinks = ['latte','espresso','cappuccino']
+coffee_machine = CoffeeMaker()
+money_machine = MoneyMachine()
+while is_machine_on:
+    my_menu = Menu()
+    user_input = input("What would you like? (espresso/latte/cappuccino): ").lower().strip()
+    if user_input == 'off':
+        is_machine_on = False
+    elif user_input == "report":
+        coffee_machine.report()
+    elif user_input in drinks:
+        the_drink = my_menu.find_drink(user_input)
+        if coffee_machine.is_resource_sufficient(the_drink):
+            make_payment = money_machine.make_payment(the_drink.cost)
+            if make_payment:
+                coffee_machine.make_coffee(the_drink)
+    elif user_input == 'refill':
+        coffee_machine = CoffeeMaker()
+    else:
+        print("That was an invalid input!")
+
