5eaba3873d09c62a086076ceca0d399f698f31f1
Ali Gad <aliahmedgad2005@gmail.com>
2025-08-15 18:35:59 +0300

    enhanced the food class
    prevented the refresh food method from generating food at the same place of the snake's segments to avoid bugs and be able to have a wining condition.

--- day21/food.py
+++ day21/food.py
@@ -12,9 +12,13 @@ class Food(Turtle):
         random_x= random.randint(-280,280)
         random_y = random.randint(-280,280)
         self.goto(random_x,random_y)
+# i think we need to avoid generating the food at a position of the snake segments also to be able to make the win condition
 
-
-    def refresh_food(self):
-        random_x= random.randint(-280,280)
-        random_y = random.randint(-280,280)
-        self.goto(random_x,random_y)
+    def refresh_food(self,snake):
+        while True:
+            random_x= random.randint(-280,280)
+            random_y = random.randint(-280,280)
+            if all(segment.distance(random_x,random_y) >=20 for segment in snake.segments):
+                self.goto(random_x,random_y)
+                break
+            
