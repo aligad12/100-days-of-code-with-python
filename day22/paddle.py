4309829977146c5475cc541118d126fee6669dcf
Ali Gad <aliahmedgad2005@gmail.com>
2025-08-17 02:16:30 +0300

    created Paddle class
    this class creates the paddles for the game and control their movement by a hold on press to move them and release

---
+++ day22/paddle.py
@@ -0,0 +1,46 @@
+from turtle import Turtle
+
+class Paddle(Turtle):
+    def __init__(self,screen):
+        super().__init__()
+        self.shape("square")
+        self.penup()
+        self.speed("fastest")
+        self.shapesize()
+        self.setheading(90)
+        self.shapesize(stretch_len=5,stretch_wid=1)
+        self.color("white")
+        self.moving_up = False
+        self.moving_down = False
+        self.screen = screen
+
+
+    def move_paddle(self,pos_x):
+        if pos_x < 390 and pos_x > -390:
+            self.goto(pos_x,0)
+        else:
+            print("that was a not allowed value!")
+            raise OverflowError
+        
+    def detect_collision(self,ball):
+        pass
+
+    def up_pressed(self):
+        self.setheading(90)
+        self.moving_up = True
+
+    def up_released(self):
+        self.moving_up = False
+
+    def down_pressed(self):
+        self.setheading(270)
+        self.moving_down = True
+
+    def down_released(self):
+        self.moving_down = False
+
+    def move_while_pressed(self):
+        if self.moving_up and self.ycor() < 220:   
+            self.goto(self.xcor(), self.ycor() + 20)
+        if self.moving_down and self.ycor() > -250:  
+            self.goto(self.xcor(), self.ycor() - 20)
