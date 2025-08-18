955b13146d2978d328698f134902e50a33824c08
Ali Gad <aliahmedgad2005@gmail.com>
2025-08-18 03:11:11 +0300

    colors file
    this file contains a list of many colors to use them using the random module to give every color a different color for better user experience.

---
+++ day23/colors.py
@@ -0,0 +1,10 @@
+colors = [
+    "white", "lightgray", "gainsboro", "silver", "whitesmoke", "ghostwhite", "aliceblue",
+    "lightsteelblue", "powderblue", "lightblue", "skyblue", "lightskyblue", "deepskyblue",
+    "aqua", "cyan", "paleturquoise", "turquoise", "mediumturquoise", "aquamarine", "mediumaquamarine",
+    "springgreen", "mediumspringgreen", "palegreen", "lightgreen", "lime", "chartreuse", "greenyellow",
+    "yellow", "lightyellow", "lemonchiffon", "khaki", "palegoldenrod", "lightgoldenrodyellow",
+    "gold", "orange", "lightsalmon", "salmon", "darksalmon", "coral", "tomato", "orangered",
+    "pink", "lightpink", "hotpink", "deeppink", "fuchsia", "magenta", "orchid", "violet",
+    "plum", "thistle", "lavender", "mediumorchid", "mediumpurple", "slateblue", "mediumslateblue"
+]
